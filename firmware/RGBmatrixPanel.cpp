89d224ab154c778c8733e61c13c30bb1358d3dcc
Robert-Jan <looy@avelon.nl>
2015-11-20 14:32:43 +0100

    changed to 64

--- firmware/RGBmatrixPanel.cpp
+++ firmware/RGBmatrixPanel.cpp
@@ -147,7 +147,7 @@ RGBmatrixPanel::RGBmatrixPanel(
 RGBmatrixPanel::RGBmatrixPanel(
   uint8_t a, uint8_t b, uint8_t c, uint8_t d,
   uint8_t sclk, uint8_t latch, uint8_t oe, boolean dbuf) :
-  Adafruit_GFX(numPanels * 32, 32) {
+  Adafruit_GFX(numPanels * 64, 32) {
   
   init(16, a, b, c, sclk, latch, oe, dbuf);
 
